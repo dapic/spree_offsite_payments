3f57818959cdf9c475f3a7f30cbf8263faa3c595
叶树剑 <yeshujian@shiguangcaibei.com>
2014-11-16 04:22:24 +0800

    now Alipay code refactured.   * code cleanup   * creates logs entries   * uses [order.number]_[payment.identifier] to identify payments sent to Alipay

--- app/controllers/spree/checkout_controller_alipay_decorator.rb
+++ app/controllers/spree/checkout_controller_alipay_decorator.rb
@@ -8,16 +8,35 @@ module Spree
       #payment_return = OffsitePayments::Integrations::Alipay::Return.new(request.query_string, @payment_method.preferred_sign)
       #unless payment_return.acknowledge
       #alipay_retrieve_order(payment_return.order)
-      if @return.is_payment_complete?
+      Rails.logger.debug("payment return is #{@return.inspect}")
+      if @order.completed?  
+        flash.notice = Spree.t(:order_processed_already)
+        redirect_to completion_route
+      elsif @return.is_payment_complete?
+        # TODO: verify this!!!
+        # @payment should have already been set. if it's nil, then we find it here
+        @payment ||= @order.payments.where(:state => ['processing', 'pending', 'checkout'], payment_method: @payment_method ).first
+        unless @payment.amount == @return.amount
+          Rails.logger.warn("payment return shows different amount than was recorded in the payment. it should be #{@payment.amount} but is actually #{@return.amount}") 
+          @payment.amount = @return.amount
+        end
+        #@payment.record_response(@return) #this creates log entries
+        @payment.log_entries.create!(:details => @return.to_yaml)
+        @payment.complete!
         #TODO: The following logic need to be revised
         #FIXME: what if the amount paid is not enough for the whole order?
-        @order.payments.where(:state => ['processing', 'pending', 'checkout']).first.complete!
-        @order.state='complete'
+        #@order.payments.where(:state => ['processing', 'pending', 'checkout']).first.complete!
+        #@order.state='complete'
+        #@order.complete!
+        @order.update_attributes(:state => "complete", :completed_at => Time.now)
         @order.finalize!
         session[:order_id] = nil
+        flash.notice = Spree.t(:order_processed_successfully)
+        Rails.logger.debug("#{@order.inspect}")
         redirect_to completion_route
       else
         #TODO: added this to tx log too
+        Rails.logger.debug("checkout #{order.number}")
         redirect_to edit_order_checkout_url(@order, :state => "payment")
       end
     end
@@ -41,19 +60,51 @@ module Spree
     def alipay_checkout_hook
       #TODO support step confirmation 
       return unless @order.next_step_complete?
-      return unless params[:order][:payments_attributes].present?
-      if @order.update_attributes(alipay_payment_params) #it would create payments
-        if params[:order][:coupon_code] and !params[:order][:coupon_code].blank? and @order.coupon_code.present?
-          fire_event('spree.checkout.coupon_code_added', :coupon_code => @order.coupon_code)
-        end
-      end
-      if alipay_pay_by_billing_integration?
-      #Rails.logger.debug "--->before alipay_handle_billing_integration"
-        alipay_handle_billing_integration
-      end
+      #return unless params[:order][:payments_attributes].present?
+
+      # all payments through alipay has these states:
+      #  * checkout -- when first created, not used
+      #  * pending  -- not used
+      #  * processing -- when redirect is made
+      #  * completed -- when we receive return/notify from Alipay and _confirmed_ it
+      #  * void  -- when price changed and this is no longer a valid payment, before "processing" #or when we did not receive return/notify in 24 hours
+      #  TODO: we should stop price from being changed after "processing"
+      #  * failed --  when we receive return/notify saying the payment failed, or _confirmation_ failed
+      #payment_method = PaymentMethod.find(params[:payment_method_id])
+      payment_method = PaymentMethod.find_by(type: Spree::BillingIntegration::Alipay)
+      payment = @order.payments.processing.find_or_create_by(amount: @order.outstanding_balance, payment_method: payment_method)
+      redirect_to alipay_full_service_url(@order, payment_method, identifier: payment.identifier)
+      #if @order.update_attributes(alipay_payment_params) #it would create payments
+      #  if params[:order][:coupon_code] and !params[:order][:coupon_code].blank? and @order.coupon_code.present?
+      #    fire_event('spree.checkout.coupon_code_added', :coupon_code => @order.coupon_code)
+      #  end
+      #end
+#      payment = @order.payments.where(state: :pending, payment_method: payment_method ).try(:first) ||
+#        @order.payments.create( amount: @order.total,
+#                                payment_method: payment_method)
+#      # update it here because the total could have changed since this payment is created
+#      payment.amount = @order.total
+#      payment.pend!
+#
+#        unless @order.payments.where(:source_type => 'Spree::BillingIntegration::Alipay').present?
+#          payment_method = PaymentMethod.find(params[:payment_method_id])
+#          skrill_transaction = SkrillTransaction.new
+#
+#          payment = @order.payments.create({:amount => @order.total,
+#                                            :source => skrill_transaction,
+#                                            :payment_method => payment_method},
+#                                            :without_protection => true)
+#          payment.started_processing!
+#          payment.pend!
+#        end
+#
+#        if alipay_pay_by_billing_integration?
+#          #Rails.logger.debug "--->before alipay_handle_billing_integration"
+#          alipay_handle_billing_integration
+#        end
     end
-    
-  private
+
+    private
 
     # This is put at the front of the filter chain for "return" and "notification" requests
     # Loading order is after this step so we have to get the payment_method by class name, instead of
@@ -62,23 +113,23 @@ module Spree
       #@payment_method = Spree::BillingIntegration::Alipay.new()
       @payment_method = Spree::PaymentMethod.find_by(type: Spree::BillingIntegration::Alipay)
       #Rails.logger.debug "key is #{@payment_method.inspect}"
-      #Rails.logger.debug "key is #{@payment_method.preferred_sign}"
+      Rails.logger.debug "#{__LINE__} key is #{@payment_method.preferred_sign}"
       case request.path_parameters[:action]
       when 'alipay_notify'
         begin
           @notification = OffsitePayments::Integrations::Alipay.notification(request.raw_post, key: @payment_method.preferred_sign)
           @notification.acknowledge
-        rescue RuntimeError => e
-          Rails.logger.warn(e)
+        rescue RuntimeError, OffsitePayments::ActionViewHelperError => e
+          Rails.logger.warn("#{e} in request: #{request.env['REQUEST_URI']}")
           render text: 'failure'
         end
       when 'alipay_done'
         begin
-        @return = OffsitePayments::Integrations::Alipay.return(request.query_string, key: @payment_method.preferred_sign) 
-        @return.acknowledge
-        rescue RuntimeError => e
-          Rails.logger.warn(e)
-          flash[:error] = Spree.t('illegal_sign_in_request')
+          @return = OffsitePayments::Integrations::Alipay.return(request.query_string, key: @payment_method.preferred_sign) 
+          @return.acknowledge
+        rescue RuntimeError, OffsitePayments::ActionViewHelperError => e
+          Rails.logger.warn("#{e} in request: #{request.env['REQUEST_URI']}")
+          flash[:error] = Spree.t('invalid_alipay_request')
           redirect_to spree.root_path
         end
       else
@@ -88,38 +139,44 @@ module Spree
 
     # This is done in the default checkout_controller filter chain
     def alipay_load_order
+      Rails.logger.debug "#{__LINE__} alipay_load_order called "
       raise "'out_trade_no' requird to load the order" unless params.key?('out_trade_no')
-      order_number = parse_alipay_out_trade_no(params['out_trade_no'])[:payment_order_number]
+      #order_number = parse_alipay_out_trade_no(params['out_trade_no'])[:payment_order_number]
+      order_number, payment_identifier = parse_alipay_out_trade_no(params['out_trade_no'])
       @order  = Order.find_by_number(order_number) #if request.referer=~/alipay.com/
+      @payment = Payment.find_by(identifier: payment_identifier)
+      #@current_order = @order
       unless @order.present?
         raise RuntimeError, "Could not find order #{order_number}"
       end
+      Rails.logger.debug "#{__LINE__} alipay_load_order called and order is found #{@order.inspect}"
     end
 
     def parse_alipay_out_trade_no(out_trade_no)
-      case 
-      when 18 == out_trade_no.length
-        return { payment_order_number: out_trade_no[0..-9], payment_identifier: out_trade_no[-8,8] }
-      when 10 == out_trade_no.length
-        return { payment_order_number: out_trade_no, payment_identifier: ''}
-      else  
-        Rails.logger.warn "Suspecious out_trade_no #{out_trade_no}"
-        return { payment_order_number: out_trade_no, payment_identifier: ''}
-      end
+      return out_trade_no.split('_')
+#      case 
+#      when 18 == out_trade_no.length
+#        return { payment_order_number: out_trade_no[0..-9], payment_identifier: out_trade_no[-8,8] }
+#      when 10 == out_trade_no.length
+#        return { payment_order_number: out_trade_no, payment_identifier: ''}
+#      else  
+#        Rails.logger.warn "Suspecious out_trade_no #{out_trade_no}"
+#        return { payment_order_number: out_trade_no, payment_identifier: ''}
+#      end
     end
-    
+
     def valid_alipay_notification?(notification, account)
       url = "https://mapi.alipay.com/gateway.do?service=notify_verify"
       result = HTTParty.get(url, query: {partner: account, notify_id: notification.notify_id}).body
       result == 'true'
     end
-    
+
     # TODO: we the code only supports "create_direct_pay_by_user" as of now (2014-11-14)
-    def alipay_full_service_url( order, alipay)
+    def alipay_full_service_url( order, alipay, identifier: nil)
       #Rails.logger.debug "alipay gateway is configured to be #{alipay.inspect}"
       raise ArgumentError, 'require Spree::BillingIntegration::Alipay' unless alipay.is_a? Spree::BillingIntegration::Alipay
       #url = OffsitePayments::Integrations::Alipay.service_url+'?'
-      helper = OffsitePayments::Integrations::Alipay::Helper.new(order.number, alipay.preferred_partner, key: alipay.preferred_sign)
+      helper = OffsitePayments::Integrations::Alipay::Helper.new([order.number, identifier].join('_'), alipay.preferred_partner, key: alipay.preferred_sign)
       #Rails.logger.debug "helper is #{helper.inspect}"
 
       if alipay.preferred_using_direct_pay_service
@@ -162,15 +219,18 @@ module Spree
       end
       return false
     end
-    
+
     # handle all supported billing_integration
     def alipay_handle_billing_integration      
-      payment_method = @order.pending_payments.first.payment_method
-      if payment_method.kind_of?(BillingIntegration::Alipay)
+      payment_method = PaymentMethod.find(params[:payment_method_id])
+      payment = @order.pending_payments.first
+      #payment_method = @order.pending_payments.first.payment_method
+      if payment.payment_method.kind_of?(BillingIntegration::Alipay)
+        #pay
         redirect_to alipay_full_service_url(@order, payment_method)
       end
     end
-    
+
     #patch spree_auth_devise/checkout_controller_decorator
     def alipay_skip_state_validation?
       %w(registration update_registration).include?(params[:state])
@@ -184,10 +244,11 @@ module Spree
   CheckoutController.send(:prepend, AlipayDecorator)
   CheckoutController.class_eval do
     SKIP_FILTERS_FOR = [:alipay_notify, :alipay_done]
-    before_action :alipay_checkout_hook, :only => [:update]
+    append_before_action :alipay_checkout_hook, :only => [:update]
     skip_before_action :verify_authenticity_token, only: SKIP_FILTERS_FOR 
     # these two actions is from spree_auth_devise
     skip_before_action :check_registration, :check_authorization, only: SKIP_FILTERS_FOR 
+    skip_before_action :load_order_with_lock, :ensure_checkout_allowed, :ensure_order_not_completed, only: SKIP_FILTERS_FOR 
     prepend_before_action :ensure_valid_alipay_request, :alipay_load_order, only: SKIP_FILTERS_FOR 
   end
 
