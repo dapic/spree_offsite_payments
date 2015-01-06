f3b604d9a81992efc32d2aa350c58207cef4a781
Stanley Yeh <stanley.yeh@gmail.com>
2015-01-06 22:06:30 +0800

    added Service to code coverage reports

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ SimpleCov.start do
   add_group 'Helpers', 'app/helpers'
   add_group 'Mailers', 'app/mailers'
   add_group 'Models', 'app/models'
+  add_group 'Services', 'app/services'
   add_group 'Views', 'app/views'
   add_group 'Libraries', 'lib'
 end
