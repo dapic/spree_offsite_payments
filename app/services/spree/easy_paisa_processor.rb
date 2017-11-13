2246f2ad2c6de47f86c02e30f0806e9cded5ce9d
Hafiz Younus <hafiz.younus@arkhitech.com>
2017-11-13 16:56:36 +0500

    require_relative in easypaisa processor to avoid isseues due to file loading order

--- app/services/spree/easy_paisa_processor.rb
+++ app/services/spree/easy_paisa_processor.rb
@@ -1,4 +1,4 @@
-require 'offsite_payments'
+require_relative './offsite_payments'
 
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
