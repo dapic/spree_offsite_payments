b80482903dbe3dc69573c26aa40e183d2fa15e2c
Hisham Malik <hisham@arkhitech.com>
2018-02-27 19:52:50 +0500

    find_by order number

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -7,16 +7,24 @@ module Spree
     include ::OffsitePayments::Integrations::EasyPaisa
     
     prepend_before_action :load_offsite_order, only: [:offsite]
+    skip_before_action :ensure_order_not_completed, only: [:offsite]
+    before_action :ensure_order_not_paid, only: [:offsite]
+    
+    def ensure_order_not_paid
+      redirect_to spree.cart_path, error: "Payment is already completed for order: #{@order.number}" if @order.paid?
+    end
+    private :ensure_order_not_paid
     
     def load_offsite_order
       if try_spree_current_user
-        @current_order = @current_api_user.orders.friendly.find(params[:order_id])                
+        @current_order = Spree::Order.find_by!(number: params[:order_id])
+        authorize! :read, @current_order
       else
-        #TODO for backward compatibility only. Should probably be removed
-        @spree_current_user = @current_api_user = Spree.user_class.find_by(spree_api_key: params[:token])
-        if @spree_current_user
-          sign_in @current_api_user
-          @current_order = @current_api_user.orders.friendly.find(params[:order_id])
+        #Currently call for offsite payments in mobiles uses token in browser to authenticate user
+        @current_spree_user = @current_api_user = Spree.user_class.find_by(spree_api_key: params[:token])
+        if @current_spree_user
+          sign_in @current_spree_user
+          @current_order = Spree::Order.accessible_by(current_ability).find_by!(number: params[:order_id])
         end
       end
     end    
@@ -24,11 +32,14 @@ module Spree
     
     #/shops/chain-mart/checkout/offsite?payment_method=ubl #&order=ordernumber
     def offsite
+      #byebug
       @payment_method = PaymentMethod.find(params[:payment_method])
-      @caller="mobile"
-      unless @order.next_step_complete?
-        render nothing: true
-      end
+      @caller = 'mobile'
+      #not requiring next_step_complete anymore as ensure_order_not_paid has been added
+#      unless @order.next_step_complete?
+#        render body: nil
+#        return
+#      end
       @payment = @order.payments.processing.find_or_create_by(amount: @order.outstanding_balance, payment_method: @payment_method)
       case @payment_method.class.name
       when Spree::BillingIntegration::UBL.name
