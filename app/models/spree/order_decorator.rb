8ba816abe16d8269b2c5dbfbe98fed067feef2e8
Hisham Malik <hisham@arkhitech.com>
2019-02-21 13:36:47 +0500

    check for cart state fix

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Order.class_eval do
   def next_step_complete?
     available_steps = checkout_steps
-    available_steps[ available_steps.index( self.state ).succ ] == 'complete'
+    !self.cart? && available_steps[ available_steps.index( self.state ).succ ] == 'complete'
   end
 end
